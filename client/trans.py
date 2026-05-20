a29cedac18fb085ae391f2760f9c47b803c104d4
M3nyus <menyhardmark04@gmail.com>
2026-05-20 14:11:44 +0200

    whisper model change

--- client/trans.py
+++ client/trans.py
@@ -23,7 +23,7 @@ SOURCE_SAMPLE_RATE = 48000  # Default for WebRTC (aiortc/browser)
 TARGET_SAMPLE_RATE = 16000  # Required by Whisper
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
-WHISPER_MODEL = "turbo"
+WHISPER_MODEL = "base"
 
 
 def transcribe_from_redis_stream(room_id, chunk_index):
